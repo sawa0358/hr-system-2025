57a48d0a81f3ab03497b4802f2ebba285b4aee54
大澤仁志 <ohsawa@MacBook-Air.local>
2025-10-04 21:06:07 +0900

    Add Prisma migrations for user fields and family members

---
+++ prisma/migrations/20251003110648_add_user_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "employees" ADD COLUMN "address" TEXT;
+ALTER TABLE "employees" ADD COLUMN "phoneInternal" TEXT;
+ALTER TABLE "employees" ADD COLUMN "phoneMobile" TEXT;
+ALTER TABLE "employees" ADD COLUMN "selfIntroduction" TEXT;
+ALTER TABLE "employees" ADD COLUMN "url" TEXT;
+ALTER TABLE "employees" ADD COLUMN "userId" TEXT;
